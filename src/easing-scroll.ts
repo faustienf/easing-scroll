2b35408e1da1d36afdf591084c118fb1c4dae999
faustienf <faustienf@protonmail.com>
2022-12-30 17:21:46 +0300

    add easing-scroll

---
+++ src/easing-scroll.ts
@@ -0,0 +1,76 @@
+type Px = number;
+type Ms = number;
+type Pct = number; // 0 - 1 (0% - 100%)
+
+type Options = {
+  target: Element;
+  top?: Px;
+  left?: Px;
+  duration?: Ms;
+  signal?: AbortSignal;
+  /**
+   * @see Easing functions https://easings.net
+   */
+  easing?: (t: Pct) => Pct;
+};
+
+const linear = (t: number): number => t;
+
+export const easingScroll = ({
+  target,
+  top,
+  left,
+  signal,
+  duration = 0,
+  easing = linear,
+}: Options): Promise<Pct> => {
+  if (signal?.aborted) {
+    return Promise.resolve(0);
+  }
+
+  if (!duration) {
+    target.scrollTo({ top, left });
+    return Promise.resolve(1);
+  }
+
+  return new Promise<Pct>((resolve) => {
+    const startTop = target.scrollTop;
+    const startLeft = target.scrollLeft;
+    const startTimestamp = performance.now();
+    let ramID: number;
+
+    const abortHandler = () => {
+      cancelAnimationFrame(ramID);
+
+      const elapsed = performance.now() - startTimestamp;
+      const progress = elapsed / duration;
+      resolve(progress);
+    };
+
+    signal?.addEventListener("abort", abortHandler);
+
+    const tick = (timestamp: DOMHighResTimeStamp) => {
+      const elapsed = timestamp - startTimestamp;
+      const progress = elapsed / duration;
+      const tickTop =
+        top === undefined
+          ? undefined
+          : startTop + (top - startTop) * easing(progress);
+      const tickLeft =
+        left === undefined
+          ? undefined
+          : startLeft + (left - startLeft) * easing(progress);
+
+      if (progress < 1) {
+        target.scrollTo({ top: tickTop, left: tickLeft });
+        ramID = requestAnimationFrame(tick);
+      } else {
+        target.scrollTo({ top, left });
+        signal?.removeEventListener("abort", abortHandler);
+        resolve(1);
+      }
+    };
+
+    ramID = requestAnimationFrame(tick);
+  });
+};
